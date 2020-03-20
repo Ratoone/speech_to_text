83d30d89ad7f6bb50ab9089da603f14436119d75
Haico Dorenbos <HaicoDorenbos@gmail.com>
2020-03-20 23:28:03 +0100

    Anova F Feature selection

--- src/FeatureSelector.py
+++ src/FeatureSelector.py
@@ -2,11 +2,14 @@ from src.FeatureExtractor import FeatureExtractor
 from typing import NamedTuple, Union, List
 import numpy as np
 import os
+import sklearn.feature_selection
 
 Datarow = NamedTuple("Datarow", [("input", np.array), ("output", Union[str, None])])
 class FeatureSelector:
     # All words in the dataset which will be used as training data
     SELECT_WORDS = ["no", "yes"]
+
+    SELECTION_P_VALUE = 1e-2
 
     # All words that we try to recognize with our machine learning algorithm
     RECOGNIZE_WORDS = {"yes", "no", "up", "down", "left", "right", "on", "off", "stop", "go"}
@@ -20,5 +23,12 @@ class FeatureSelector:
                 file_path = folder_path + "/" + file
                 if file_path.endswith(".npy"):
                     data.append(Datarow(np.load(file_path), word))
+
+        X = np.array([d.input for d in data])
+        y = np.array([d.output for d in data])
+        _, p_values = sklearn.feature_selection.f_classif(X, y)
+        selection_indices = [i for i, p in enumerate(p_values) if p <= self.SELECTION_P_VALUE]
+        for i, d in enumerate(data):
+            data[i] = Datarow(np.take(d.input, selection_indices), d.output)
 
         return data
